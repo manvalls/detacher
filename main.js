99818d101479cbe8b7cefe19fa0dbb282381ed65
Manuel Valls Fernández <manolo@vlrz.es>
2015-08-04 12:03:38 +0200

    Added callback support

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 var define = require('u-proto/define'),
-    
+
+    args = Symbol(),
     active = Symbol();
 
 function Detacher(){
   this[active] = true;
+  this[args] = arguments;
 }
 
 Detacher.prototype[define]({
-  
+
   get active(){
     return this[active];
   },
-  
+
   detach: function(){
+    var a;
+
+    if(!this[active]) return;
+
     this[active] = false;
+    a = this[args];
+
+    if(a.length) try{ a[0].apply(a[2] || this,a[1] || []); }
+    catch(e){ setTimeout(throwError,0,e); }
   }
-  
+
 });
+
+function throwError(e){
+  throw e;
+}
 
 module.exports = Detacher;
