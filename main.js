a19ea860c7c80b1da15d889f94f086d8a1cd66ba
Manuel Valls Fernández <manolo@vlrz.es>
2016-12-04 11:35:23 +0100

    Rely on getter collection capabilities

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 /**/ 'use strict' /**/
 var resolver = Symbol(),
     args = Symbol(),
-    col = Symbol(),
-    Resolver,Setter,define;
+    Resolver,define;
 
 module.exports = Detacher;
 Resolver = require('y-resolver');
-Setter = require('y-setter');
 define = require('u-proto/define');
 
 function Detacher(){
   Resolver.Yielded.call(this,resolver);
   this[args] = arguments;
-  this[col] = new Set();
 }
 
 Detacher.prototype = Object.create(Resolver.Yielded.prototype);
@@ -25,40 +22,17 @@ Detacher.prototype[define]({
   },
 
   detach: function(){
-    var a,d;
+    var a;
 
     if(this.done) return;
 
     a = this[args];
+    delete this[args];
+
     if(a.length) try{ a[0].apply(a[2] || this,a[1] || []); }
     catch(e){ setTimeout(throwError,0,e); }
 
     this[resolver].accept();
-    for(d of this[col]) detach(d);
-    this[col].clear();
-  },
-
-  add: function(){
-    var d;
-
-    if(this.done){
-      for(d of arguments) detach(d);
-      return;
-    }
-
-    for(d of arguments){
-      this[col].add(d);
-      if(Resolver.Yielded.is(d)) d.listen(this[col].delete,[d],this[col]);
-      else if(Resolver.is(d)) d.yielded.listen(this[col].delete,[d],this[col]);
-      else if(Setter.Getter.is(d)) d.frozen().listen(this[col].delete,[d],this[col]);
-      else if(Setter.is(d)) d.getter.frozen().listen(this[col].delete,[d],this[col]);
-    }
-
-  },
-
-  remove: function(){
-    var d;
-    for(d of arguments) this[col].delete(d);
   }
 
 });
@@ -67,20 +41,6 @@ Detacher.prototype[define]({
 
 function throwError(e){
   throw e;
-}
-
-function detach(d){
-  d = d || {};
-
-  if(d.detach) return d.detach();
-  if(d.disconnect) return d.disconnect();
-  if(d.close) return d.close();
-  if(d.kill) return d.kill();
-  if(d.pause) return d.pause();
-  if(d.freeze) return d.freeze();
-  if(d.accept) return d.accept();
-  if(d.reject) return d.reject();
-
 }
 
 module.exports.prototype = Detacher.prototype;
